13d8d9888289c9e7209eb063ddd56e3b8538f690
yni58-ni <nicole.niyayu@gmail.com>
2022-10-13 00:52:31 -0400

    second commit

---
+++ cypress/e2e/spec.cy.js
@@ -0,0 +1,28 @@
+describe('empty spec', () => {
+  it('passes', () => {
+    cy.visit('https://proman-delta.vercel.app/login')
+    cy.get('[name=email]')
+      .type('shine.sood91@gmail.com')
+      .should('have.value','shine.sood91@gmail.com')
+
+      cy.get('[name=password]')
+      .type('Legendary')
+      .should('have.value','Legendary')
+
+      cy.get('button:contains("Login")').click().should('be.enabled')
+  })
+
+  it('fail',()=>{
+    cy.visit('https://proman-delta.vercel.app/login')
+    cy.get('[name=email]')
+      .type('shine.sood91@gmail.com')
+      .should('have.value','shine.sood91@gmail.com')
+
+      cy.get('[name=password]')
+      .type('wrrrrrrong')
+      .should('have.value','wrrrrrrong')
+
+      cy.get('button:contains("Login")').click().should('be.disabled')
+  })
+
+})
